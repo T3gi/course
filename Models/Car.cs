c235490de150c2baf8129496ed0d2ad38dd21ec6
T3gi <tytok.volodymyr@lll.kpi.ua>
2025-03-24 13:47:47 +0200

    Add project files.

---
+++ Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Phoenix.Models;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Mark { get; set; }
+    public decimal Price { get; set; }
+}
